bd2af22d09c8489b48d5595a2137647314d606cc
wjdgurrj <wjdgurrj25@gmail.com>
2021-01-15 17:37:30 +0900

    login 서버 구축 및 join시 password 일치성 확인

--- GADA/express.js
+++ GADA/express.js
@@ -3,37 +3,43 @@ const app = express();
 const path = require("path");
 const request = require('request');
 var mysql = require('mysql');
+var jwt = require('jsonwebtoken');
 
 var connection = mysql.createConnection({ // db 연결하기
-    host: '192.168.0.26',
-    user: 'newuser',
-    password: 'ljh9915!',
-    database: 'gada'
+  host: '192.168.0.26',
+  user: 'newuser',
+  password: 'ljh9915!',
+  database: 'gada'
 });
 connection.connect();
 
-  
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // urlencoded 타입의 데이터를 받기 위한 설정
-
-//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
-  });
-
-app.get("/join", function(req,res){
-    res.render("join");
+  res.send('Hello World')
 });
 
-app.post('/join', function(req,res){
-    var userId = req.body.userId;
-    var userPassword = req.body.userPassword;
-    console.log(userId, userPassword);
-    connection.query("INSERT INTO `userinfo`(`userId`,`userPw`) VALUES(?,?);",
+app.get("/join", function (req, res) {
+  res.render("join");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.post('/join', function (req, res) {
+  var userId = req.body.userId;
+  var userPassword = req.body.userPassword;
+  var checkPassword = req.body.checkPassword;
+  console.log(userId, userPassword);
+  if(userPassword == checkPassword){
+  connection.query("INSERT INTO `userinfo`(`userId`,`userPw`) VALUES(?,?);",
     [
       userId,
       userPassword,
@@ -41,8 +47,39 @@ app.post('/join', function(req,res){
     function (error, results, fields) {
       if (error) throw error;
       else {
-        res.json(1);
+          res.json("성공");
       }
     });
+  }
+  else{
+    res.json("실패");
+  }
 });
-app.listen(3000)
+
+app.post('/login', function (req, res) {
+  var userId = req.body.userId;
+  var userPassword = req.body.userPassword;
+  console.log(userId, userPassword);
+  connection.query("SELECT * FROM userinfo WHERE userId = ?", [userId], function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    else {
+      if (results.length == 0) {
+        res.json(2); // 아이디 존재하지 않음
+      } else {
+        var storedPassword = results[0].userPw;
+        if (storedPassword == userPassword) {
+          console.log("로그인 성공");
+          res.json("로그인 성공");
+        } else {
+          console.log("로그인 실패");
+          res.json("로그인 실패");
+        }
+      }
+    }
+  });
+});
+app.listen(3002)
